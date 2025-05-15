f96497eb0b64863e67a6bbe333c1d296bdeb6c0d
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-15 16:53:02 -0300

    initial set up and apply the tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 
+
 function App() {
 
   return (
     <>
       <main>
-        <h1>Seja bem vindo! :)</h1>
+        <h1 className='text-3xl font-bold underline'>Seja bem vindo! :)</h1>
         <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
       </main>
     </>
